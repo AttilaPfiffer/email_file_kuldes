c43080f76ee3a03b9c8b0428234de5ed408a001e
AttilaPfiffer <pifuati@gmail.com>
2024-10-22 13:51:31 +0200

    api

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,7 @@ use App\Http\Controllers\MailController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
 
-Route::get('send-mail', [MailController::class, 'index']);
-
-Route::get('file-upload', [FileController::class, 'index']);
-Route::post('file-upload', [FileController::class, 'store'])->name('file.store');
+require __DIR__.'/auth.php';
